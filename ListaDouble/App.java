6d4cc15501a6f723856922e97ebe06578cf61e52
Mariana Nunes <mariana-nunes6@hotmail.com>
2025-09-16 14:12:44 +0000

    aulas metodos get set remove

---
+++ ListaDouble/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+       DoubleLinkedListOfInteger dp = new DoubleLinkedListOfInteger();
+        dp.add(4);
+        dp.add(5);
+        dp.add(8);
+        dp.toString();
+        dp.remove(8);
+        dp.toString();
+
+    }
+}
--- ListaDouble/DoubleLinkedListOfInteger.java
+++ ListaDouble/DoubleLinkedListOfInteger.java
@@ -1,3 +1,5 @@
+
+
 public class DoubleLinkedListOfInteger {
     private class Node{
         public Integer element;
@@ -86,6 +88,91 @@ public class DoubleLinkedListOfInteger {
         n.next = trailer;
         trailer.prev.next = n;
         trailer.prev = n;
+    }
+
+    //remover nodo
+    //da esquerda para a direita estou navegando e na direita estou atribuindo valores
+    //aux.prev.next = aux.next
+    //aux.next.prev = aux.prev
+    public boolean remove(Integer element){
+        Node aux = header.next;//header agr é um sentinela
+        for(int i =0; i<cont; i++){
+            if (aux.element.equals(element)){
+                aux.prev.next=aux.next;
+                aux.next.prev=aux.prev;
+                cont--;
+                return true;    
+            }
+            aux=aux.next;
+        }
+        return false;   
+    }
+
+    private Node getNodeIndex(int index){
+        Node aux = null;//n inicializar com o header pq ainda n sabemos a posição do node a ser buscado
+        if (index <= cont/2){//vendo onde é o meio da lista para ver ainde comçamos a buscar, header ou trailer
+            aux=header.next;//fazendo o percurso do inicio para o fim depois da verificação do if
+            for (int i = 0; i < index; i++) {
+                aux= aux.next;
+            }
+        }else{
+            aux=trailer.prev;
+            for (int i = cont-1; i > index; i--) {
+                aux=aux.prev;
+            }
+        }
+        return aux;
+    }
+
+    public void add (int index, Integer element){
+        if(index < 0 || index > cont){
+            throw new IndexOutOfBoundsException("Erro, index inválido!");
+        }else if(index == cont){
+            add(element);
+        }else{
+            Node n = new Node(element);
+            Node aux = getNodeIndex(index);
+            n.next = aux.prev;
+            n.prev = aux.prev.next;
+            aux.prev.next = n.prev;//acho q esta muito errado esse e o de baixo
+            aux.prev = n.next; 
+        }
+        cont++;
+    }
+
+    public Integer get (int index){
+        if(index < 0 || index >= cont){
+            throw new IndexOutOfBoundsException("Erro, index inválido!");
+        }else{
+            Node aux = getNodeIndex(index);
+            Integer element = aux.element;
+            return element;
+        }
+    }
+
+    public Integer set(int index, Integer novo){
+        if(index < 0 || index >= cont){
+            throw new IndexOutOfBoundsException("Erro, index inválido!");
+        }else{
+            Node aux = getNodeIndex(index);
+            Integer auxE = aux.element;
+            aux.element = novo;
+            return auxE;
+        }
+    }
+
+    public Integer removeByIndex(int index){
+        if (index < 0 || index >= cont){
+            throw new IndexOutOfBoundsException("Erro, index inválido!");
+        }else{
+            Node aux = getNodeIndex(index);
+                aux.prev.next=aux.next;
+                aux.next.prev=aux.prev;
+                cont--;
+            Node auxn = getNodeIndex(index);
+            Integer auxE = auxn.element;
+            return auxE; 
+        }
     }
 
 
