d2c63f8bbdbd377105e89ec44a8b6ba33b2c49f7
Mariana Nunes <mariana-nunes6@hotmail.com>
2025-09-18 14:12:40 +0000

    Pilha pronta

--- pilha/src/main/java/com/nunes/App.java
+++ pilha/src/main/java/com/nunes/App.java
@@ -23,18 +23,37 @@ public class App {
     // exemplo: vetor original 1 2 3 4  vetor invertido 4 3 2 1
     // ==> utilizar pilhas para realizar a inversão
     public static void inverte(Integer vet[]) {
-        
+        Pilha aux = new Pilha();
+        for (int i = 0; i < vet.length; i++) {
+            aux.push(vet[i]);
+            vet[i]=0;
+        }
+        for (int i = 0; i < vet.length; i++) {
+            vet[i]=aux.pop();
+        }    
     }
 
     // recebe a pilha preenchida e retorna uma pilha exatamente igual a que foi recebida por parâmetro
     // utilizar pilhas para realizar a clonagem 
     public static Pilha getClone(Pilha p) {
-        return null;
+        Pilha aux = new Pilha();
+        Pilha auxx = new Pilha();
+        for (int i = 0; i < p.size(); i++) {
+            aux.push(p.pop());
+        }
+        for (int i = 0; i < aux.size(); i++) {
+            auxx.push(aux.pop());
+        }
+        return auxx;
     }
     
     // recebe uma pilha preenchida e uma lista vazia
     // move os elementos da pilha para a lista mantendo a ordem da pilha
     public static void addAll(Pilha p, LinkedListOfInteger l) {
-        
+        Pilha aux = new Pilha();
+        for (int i = 0; i < aux.size(); i++) {
+            aux.push(p.pop());
+            l.add(aux.pop());
+        }        
     }
 }
