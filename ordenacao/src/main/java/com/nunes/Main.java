4599319fceea429cdccdf37d39223b2bd35f2769
Mariana Nunes <mariana-nunes6@hotmail.com>
2025-10-09 14:12:13 +0000

    terminar atividade

--- ordenacao/src/main/java/com/nunes/Main.java
+++ ordenacao/src/main/java/com/nunes/Main.java
@@ -2,6 +2,15 @@ package com.nunes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int vetor[] = {85,24,63,45,17,31,96,50};
+        for (int i=0; i<vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+        InsertionSort is = new InsertionSort();   //teste insertion sort
+        is.sort(vetor);
+        for (int i=0; i<vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }    
     }
 }
